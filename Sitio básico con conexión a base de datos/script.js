e25ca5fe91282bc5abcf8f252554158434a883b7
Hector Arciniega <125413020+HAV2023@users.noreply.github.com>
2025-03-06 22:39:37 -0600

    Delete Sitio básico con conexión a base de datos/script.js

--- Sitio básico con conexión a base de datos/script.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const restrictedMenu = document.getElementById('restricted-menu');
-
-    if (sessionStorage.getItem('loggedIn')) {
-        restrictedMenu.style.display = 'block';
-    }
-});
