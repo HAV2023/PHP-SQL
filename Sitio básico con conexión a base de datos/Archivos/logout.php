bed8c3d70564cfeee402bc2b7db60ddafdbb72f8
Hector Arciniega <125413020+HAV2023@users.noreply.github.com>
2025-03-08 11:01:30 -0600

    Update logout.php

--- Sitio básico con conexión a base de datos/Archivos/logout.php
+++ Sitio básico con conexión a base de datos/Archivos/logout.php
@@ -1,6 +1,13 @@
 <?php
+// Iniciar la sesión para poder acceder a las variables de sesión existentes
 session_start();
+
+// Destruir la sesión actual, eliminando todas las variables de sesión almacenadas
 session_destroy();
+
+// Redirigir al usuario a la página de inicio (index.html) después de cerrar sesión
 header("Location: index.html");
+
+// Finalizar la ejecución del script para evitar que se ejecute código adicional después de la redirección
 exit;
 ?>
